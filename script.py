95968ec083cdd253820ddeb9cda2183b5eaa0b10
t_fraser <david.rt.fraser@gmail.com>
2019-06-30 13:26:08 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,27 @@
-import csv
 import datetime
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from time import sleep
+from random import randint,shuffle
+#from fake_useragent import UserAgent
 
 def get_html(url):
     html_content = ''
-    try:
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        html_page = urlopen(req).read()
-        html_content = BeautifulSoup(html_page, "html.parser")
-    except: 
-        pass
+    #try:
+    req = Request(url, headers={'User-Agent':'Mozilla/5.0'}, timeout=60)
+    html_page = urlopen(req).read()
+    html_content = BeautifulSoup(html_page, "html.parser")
+    '''except: 
+        try:
+            print('long sleep')
+            sleep(randint(800,1500))
+            req = Request(url, headers={'User-Agent':'Mozilla/5.0'}, timeout=60)
+            html_page = urlopen(req).read()
+            html_content = BeautifulSoup(html_page, "html.parser")
+        except:
+            pass
         
+    '''
     return html_content
 
 def get_category_items(category_url):
@@ -22,7 +32,7 @@ def get_category_items(category_url):
             items.append(item.get('href'))
     except: 
         pass
-
+    shuffle(items)
     return items
 
 def get_categories(url):
@@ -35,7 +45,7 @@ def get_categories(url):
             items.append(item)
     except: 
         pass
- 
+    shuffle(items)
     return items
 
 def get_details(url):
@@ -69,7 +79,7 @@ def get_details(url):
 
     try:
         raw_text = html.find_all("div", {"class":"woocommerce-product-details__short-description"})[0].get_text()
-        stamp['raw_text'] = raw_text.replace('¬†','.') # I believe uft-8 isn't the right encoding for this website.
+        stamp['raw_text'] = raw_text
     except:
         stamp['raw_text'] = None
 
@@ -80,18 +90,15 @@ def get_details(url):
     except:
         stamp['category'] = None
 
-    currency = "USD"
-    stamp['currency'] = currency
+    stamp['currency'] = 'USD'
 
     # image_urls should be a list
-    images = ""
+    images = []
     try:
         image_items = html.find_all('div', {'class': 'woocommerce-product-gallery__image'})
         for image_item in image_items:
             img = image_item.find('a').get('href')
-            if(images):
-                images = images + ','
-            images = images + img
+            images.append(img)
     except:
         pass
 
@@ -102,15 +109,9 @@ def get_details(url):
     stamp['scrape_date'] = scrape_date
 
     stamp['url'] = url
-
+    print(stamp,'+++++++++++++')
+    sleep(randint(22,99))
     return stamp
-
-# empty csv output file and write header
-f = csv.writer(open('results.csv', 'w', encoding="utf8", newline=""))
-f.writerow(['Name', 'Price', 'Currency','SKU', 'Scrape date', 'Category' , 'Raw text' , 'Image Urls' , 'Link'])
-
-# initialize stamps array
-stamps = []
 
 # start url
 start_url = 'https://kentonphilatelics.com/product-category/stamps/'
@@ -126,8 +127,4 @@ for category in categories:
         for category_item in category_items:
             # get current item details 
             stamp = get_details(category_item)
-            if stamp:
-                stamps.append(stamp)
-                # write current item details in output csv 
-                f.writerow([stamp['title'], stamp['price'], stamp['currency'], stamp['sku'], stamp['scrape_date'], stamp['category'], stamp['raw_text'], stamp['image_urls'], stamp['url']])
          
